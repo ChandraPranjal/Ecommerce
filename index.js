3f54b774f018e457e5dcb283af6a6c34c4cc11f4
ChandraPranjal <iampranjalchandra@gmail.com>
2023-10-22 20:54:35 +0530

    progress

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+
+app.listen(process.env.PORT|| 5000 , ()=>{
+    console.log("Server is listening...")
+})
+
